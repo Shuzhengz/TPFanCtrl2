bea417d1f8ed83b063e22d11826068c44cf52f18
FanDjango <51046875+FanDjango@users.noreply.github.com>
2025-01-30 21:15:11 +0100

    Use parameter ManModeExit parameter instead of constant "75"

--- fancontrol/fanstuff.cpp
+++ fancontrol/fanstuff.cpp
@@ -323,7 +323,8 @@ FANCONTROL::HandleData(void) {
 	}
 
 	this->PreviousMode = this->CurrentMode;
-	if (this->CurrentMode == 3 && this->MaxTemp > 75) this->CurrentMode = 2; //hello
+
+	if (this->CurrentMode == 3 && this->MaxTemp > this->ManModeExit2) this->CurrentMode = 2;
 
 	return ok;
 }
